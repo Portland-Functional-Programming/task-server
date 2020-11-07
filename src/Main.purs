1336c84c390d73f0a8b28812e57225984c74cd35
Tim McIver <tim@timmciver.com>
2020-11-06 19:29:21 -0500

    Add basic server implementation using HTTPure.

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,11 @@ module Main where
 
 import Prelude
 
-import Effect (Effect)
 import Effect.Console (log)
+import HTTPure as HTTPure
 
-main :: Effect Unit
-main = do
-  log "🍝"
+main :: HTTPure.ServerM
+main =
+  HTTPure.serve 8080 router $ log "Server now up on port 8080"
+  where
+    router _ = HTTPure.ok "hello world!"
