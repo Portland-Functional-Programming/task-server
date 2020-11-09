2bda2854ef8d04758efb0e89f0afb8b798c69bbe
Tim McIver <tim@timmciver.com>
2020-11-08 22:00:47 -0500

    Serve static files. Add a css file.

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,18 @@
 module Main where
 
-import Prelude (map, show, ($), (<<<), (<>))
+import Prelude
 
 import Effect.Console (log)
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Class (liftEffect)
 import HTTPure as HTTPure
+import HTTPure ((!@), Response)
 import Task (Priority(..), Tag(..), Task)
 import View.Tasks (renderTasks)
+import Data.Array (tail)
 import Data.Foldable (intercalate)
+import Data.Maybe (Maybe(..))
+import Node.FS.Aff as FS
 
 unlines :: Array String -> String
 unlines = intercalate "\n"
@@ -22,10 +28,14 @@ tasks = [ { name: "Buy milk"
           }
         ]
 
+serveStaticFile :: forall m. MonadAff m => String -> m Response
+serveStaticFile path = liftAff $ FS.readFile path >>= HTTPure.ok
+
 main :: HTTPure.ServerM
 main =
   HTTPure.serve 8080 router $ log "Server now up on port 8080"
   where
     router { path: [] } = HTTPure.permanentRedirect' (HTTPure.header "Location" "/tasks") ""
     router { path: ["tasks"] } = HTTPure.ok $ renderTasks tasks
+    router { path } | path !@ 0 == "static" = (liftEffect $ log ("path: " <> show path)) *> (serveStaticFile (intercalate "/" path))
     router _ = HTTPure.notFound
--- src/View/Tasks.purs
+++ src/View/Tasks.purs
@@ -4,7 +4,7 @@ import Data.Foldable (for_)
 import Prelude (($), Unit, discard, show)
 import Task
 import Text.Smolder.HTML
-import Text.Smolder.HTML.Attributes (lang)
+import Text.Smolder.HTML.Attributes (lang, href, rel)
 import Text.Smolder.Markup ((!), text)
 import Text.Smolder.Renderer.String (render)
 
@@ -15,10 +15,12 @@ renderTask task = tr $ do
 
 renderTasks :: Array Task -> String
 renderTasks tasks = render doc
-  where doc = html ! lang "en" $ body $ do
-          h1 $ text "Your Tasks"
-          table $ do
-            thead $ tr $ do
-              th $ text "Task Name"
-              th $ text "Priority"
-            tbody $ for_ tasks renderTask
+  where doc = html ! lang "en" $ do
+          head $ link ! href "/static/css/tasks.css" ! rel "stylesheet"
+          body $ do
+            h1 $ text "Your Tasks"
+            table $ do
+              thead $ tr $ do
+                th $ text "Task Name"
+                th $ text "Priority"
+              tbody $ for_ tasks renderTask
