e6be1dee4e77d48184a332139b4fbcf14581cadb
Tim McIver <tim@timmciver.com>
2020-11-14 09:14:27 -0500

    Create a task controller and move code there.

---
+++ src/Controller/Task.purs
@@ -0,0 +1,54 @@
+module Controller.Task (post) where
+
+import Prelude
+
+import Data.Array ((:), head, tail, filter)
+import Data.Bifunctor as Bifunctor
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
+import Data.String (split, joinWith, Pattern(..))
+import Effect.Aff.Class (class MonadAff)
+import Effect.Class (liftEffect)
+import Effect.Ref as Ref
+import Foreign.Object as Object
+import HTTPure as HTTPure
+import HTTPure.Utils as Utils
+import HTTPure ((!!), Response)
+import Task (Priority(..), Task)
+
+toPriority :: String -> Maybe Priority
+toPriority "low" = Just Low
+toPriority "medium" = Just Medium
+toPriority "high" = Just High
+toPriority _ = Nothing
+
+-- | Copied with modification from
+-- https://github.com/cprussin/purescript-httpure/blob/a81abca2d64bd9805874c4a2a80c07144fd19d11/src/HTTPure/Query.purs#L29
+parse :: String -> HTTPure.Query
+parse = split' "&" >>> nonempty >>> toObject
+  where
+    toObject = map toTuple >>> Object.fromFoldable
+    nonempty = filter ((/=) "")
+    split' = Pattern >>> split
+    first = head >>> fromMaybe ""
+    last = tail >>> fromMaybe [] >>> joinWith ""
+    decode = Utils.replacePlus >>> Utils.urlDecode
+    decodeKeyValue = Bifunctor.bimap decode decode
+    toTuple item = decodeKeyValue $ Tuple (first itemParts) (last itemParts)
+      where
+        itemParts = split' "=" item
+
+post :: forall m. MonadAff m => Ref.Ref (Array Task) -> String -> m Response
+post tasksRef reqBody = case maybeTask of
+  Just task -> do
+    liftEffect $ Ref.modify_ (task : _) tasksRef
+    HTTPure.seeOther' (HTTPure.header "Location" "/tasks") ""
+  Nothing -> HTTPure.badRequest "Unable to create a new task."
+  where maybeTask = do
+          let params = parse reqBody
+          name <- params !! "name"
+          priority <- params !! "priority" >>= toPriority
+          pure { name: name
+               , priority: priority
+               , tags: []
+               }
--- src/Main.purs
+++ src/Main.purs
@@ -2,20 +2,14 @@ module Main where
 
 import Prelude
 
-import Data.Array ((:), head, tail, filter)
-import Data.Bifunctor as Bifunctor
+import Controller.Task as TaskController
 import Data.Foldable (intercalate)
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.String (split, joinWith, Pattern(..))
-import Data.Tuple (Tuple(..))
 import Effect.Aff.Class (class MonadAff, liftAff)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
 import Effect.Ref as Ref
-import Foreign.Object as Object
-import HTTPure ((!!), (!@), Response)
+import HTTPure ((!@), Response)
 import HTTPure as HTTPure
-import HTTPure.Utils as Utils
 import Node.FS.Aff as FS
 import Task (Priority(..), Tag(..), Task)
 import View.Tasks (renderTasks)
@@ -31,45 +25,8 @@ tasks = [ { name: "Buy milk"
           }
         ]
 
-toPriority :: String -> Maybe Priority
-toPriority "low" = Just Low
-toPriority "medium" = Just Medium
-toPriority "high" = Just High
-toPriority _ = Nothing
-
 serveStaticFile :: forall m. MonadAff m => String -> m Response
 serveStaticFile path = liftAff $ FS.readFile path >>= HTTPure.ok
-
--- | Copied with slight modification from
--- https://github.com/cprussin/purescript-httpure/blob/a81abca2d64bd9805874c4a2a80c07144fd19d11/src/HTTPure/Query.purs#L29
-parse :: String -> HTTPure.Query
-parse = split' "&" >>> nonempty >>> toObject
-  where
-    toObject = map toTuple >>> Object.fromFoldable
-    nonempty = filter ((/=) "")
-    split' = Pattern >>> split
-    first = head >>> fromMaybe ""
-    last = tail >>> fromMaybe [] >>> joinWith ""
-    decode = Utils.replacePlus >>> Utils.urlDecode
-    decodeKeyValue = Bifunctor.bimap decode decode
-    toTuple item = decodeKeyValue $ Tuple (first itemParts) (last itemParts)
-      where
-        itemParts = split' "=" item
-
-createTask :: forall m. MonadAff m => Ref.Ref (Array Task) -> String -> m Response
-createTask tasksRef reqBody = case maybeTask of
-  Just task -> do
-    liftEffect $ Ref.modify_ (task : _) tasksRef
-    HTTPure.seeOther' (HTTPure.header "Location" "/tasks") ""
-  Nothing -> HTTPure.badRequest "Unable to create a new task."
-  where maybeTask = do
-          let params = parse reqBody
-          name <- params !! "name"
-          priority <- params !! "priority" >>= toPriority
-          pure { name: name
-               , priority: priority
-               , tags: []
-               }
 
 main :: HTTPure.ServerM
 main = do
@@ -80,6 +37,6 @@ main = do
     router tasksRef { path: ["tasks"], method: HTTPure.Get } = do
       tasks' <- liftEffect $ Ref.read tasksRef
       HTTPure.ok $ renderTasks tasks'
-    router tasksRef { path: ["tasks"], method: HTTPure.Post, body } = createTask tasksRef body
+    router tasksRef { path: ["tasks"], method: HTTPure.Post, body } = TaskController.post tasksRef body
     router _ { path } | path !@ 0 == "static" = serveStaticFile (intercalate "/" path)
     router _ _ = HTTPure.notFound
